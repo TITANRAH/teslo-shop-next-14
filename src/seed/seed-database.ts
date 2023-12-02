ca5d5f0895464b10bff5b935fe8be82c45e2b75e
Sergio Miranda <granrah1@gmail.com>
2023-12-02 01:26:13 -0300

    prisma

---
+++ src/seed/seed-database.ts
@@ -0,0 +1,16 @@
+import { initialData } from "./seed";
+import prisma from "../lib/prisma";
+
+async function main() {
+  // 1. Borrar registros previos
+  await Promise.all([
+    prisma.productImage.deleteMany(),
+    prisma.product.deleteMany(),
+    prisma.category.deleteMany(),
+  ]);
+}
+
+(() => {
+  if (process.env.NODE_ENV === "production") return;
+  main();
+})();
