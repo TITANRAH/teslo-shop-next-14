ab1defa8e5e3387681bd520cad5a5fef85bdb53b
Sergio Miranda <granrah1@gmail.com>
2023-12-02 14:01:35 -0300

    pagination

--- src/utils/generatePaginationNumbers.ts
+++ src/utils/generatePaginationNumbers.ts
@@ -10,25 +10,25 @@ export const generatePagination = (currentPage: number, totalPages: number) => {
   // mostrar las primeras 3, puntos suspensivos, y las ultimas 2
   if (currentPage <= 3) {
     return [1, 2, 3, "...", totalPages - 1, totalPages]; //si fueran 50 paginas [1,2,3, '..., 49, 50]
-
-    // si la pagina actual esta entre las ultimas 3 paginas
-    // mostrar las primeras 2, puntos suspesivos, las ultimas 3 paginas
-
-    if (currentPage >= totalPages - 2) {
-      return [1, 2, "...", totalPages - 2, totalPages - 1, totalPages];
-    }
-
-    // si la pagina actual esta en otro lugar medio
-    // mostrar la priemra pagina, puntos suspensivos, la pagina actual y vecinos
-
-    return [
-      1,
-      "...",
-      currentPage - 1,
-      currentPage,
-      currentPage + 1,
-      "...",
-      totalPages,
-    ];
   }
+
+  // si la pagina actual esta entre las ultimas 3 paginas
+  // mostrar las primeras 2, puntos suspesivos, las ultimas 3 paginas
+
+  if (currentPage >= totalPages - 2) {
+    return [1, 2, "...", totalPages - 2, totalPages - 1, totalPages];
+  }
+
+  // si la pagina actual esta en otro lugar medio
+  // mostrar la priemra pagina, puntos suspensivos, la pagina actual y vecinos
+
+  return [
+    1,
+    "...",
+    currentPage - 1,
+    currentPage,
+    currentPage + 1,
+    "...",
+    totalPages,
+  ];
 };
